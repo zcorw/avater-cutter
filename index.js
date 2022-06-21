a2be7ff3fd5eb0f35364b85d8a34a7a3d05ffb37
zcorw <zhzhwwt@gmail.com>
2022-06-21 11:16:58 +0800

    图片拖动

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function init() {
     y,
     width,
     height,
-    scale, 
+    scale,
   }
 }
 // 绘制到画布
@@ -60,22 +60,62 @@ function zoomImage(imgData, scale) {
     height: convertH,
   }
 }
+// 拖拽图片
+let imgActive = false;
+let dragPoint = { x: 0, y: 0 };
+function dragImgOn(e) {
+  imgActive = true;
+  dragPoint = {
+    pageX: e.pageX,
+    pageY: e.pageY,
+    x: imgData.x,
+    y: imgData.y,
+  };
+}
+function dragImgOff(e) {
+  imgActive = false;
+}
+function dragImg(e) {
+  e.preventDefault();
+  const offset = {
+    x: e.pageX - dragPoint.pageX,
+    y: e.pageY - dragPoint.pageY,
+  };
+  return {
+    ...imgData,
+    x: dragPoint.x + offset.x,
+    y: dragPoint.y + offset.y,
+  }
+}
 
 let imgData;
-img.onload = function() {
+img.onload = function () {
   imgData = init();
   drawToCanvas(imgData);
 }
-document.getElementById('plus').addEventListener('click', function() {
-  const scale = imgData.scale + 0.02;
-  imgData = zoomImage(imgData, scale);
+function _zoom(scale) {
+  const _scale = imgData.scale + scale;
+  imgData = zoomImage(imgData, _scale);
   drawToCanvas(imgData);
-});
-document.getElementById('minus').addEventListener('click', function() {
-  const scale = imgData.scale - 0.02;
-  imgData = zoomImage(imgData, scale);
-  drawToCanvas(imgData);
-});
-document.getElementById('cvsWrap').onmousewheel = function (e) {
-  console.log(e.wheelDelta)
 }
+document.getElementById('plus').addEventListener('click', function () {
+  _zoom(0.02);
+});
+document.getElementById('minus').addEventListener('click', function () {
+  _zoom(-0.02);
+});
+cvs.addEventListener('wheel', function (e) {
+  const _e = e || window.event;
+  const scale = _e.wheelDelta / 6000;
+  _zoom(scale);
+  e.preventDefault();
+});
+cvs.addEventListener('mousedown', dragImgOn);
+cvs.addEventListener('mouseup', dragImgOff);
+cvs.addEventListener('mousemove', function (e) {
+  console.log(e.pageX, e.pageY)
+  if (imgActive) {
+    imgData = dragImg(e);
+    drawToCanvas(imgData);
+  }
+});
